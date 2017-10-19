481eed5a0e8fb9e80678d7b09ed945e567b34305
Gkimura <kyabetsu.3141@gmail.com>
2017-10-19 16:58:18 +0900

    特に何もない。次は多変数関数

--- main.cpp
+++ main.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 //#include <Eigen/Dense>
 #include "AutoDiff.hpp"
-#include "DualNum.hpp"
+//#include "DualNum.hpp"
 
 
 int main()
 {
-    double a = 2;
+    double a = 3.141592653589793238462643383;
 
     auto f = [](auto x) { return std::sin(x); };
 
-    std::cout << Diff<double, 0>()(f, a) << std::endl;
-    std::cout << Diff<double, 1>()(f, a) << std::endl;
-    std::cout << Diff<double, 2>()(f, a) << std::endl;
-    std::cout << Diff<double, 3>()(f, a) << std::endl;
-    std::cout << Diff<double, 4>()(f, a) << std::endl;
+    std::cout << Diff<double,0>()(f, a) << std::endl;
+    std::cout << Diff<double,1>()(f, a) << std::endl;
+    std::cout << Diff<double,2>()(f, a) << std::endl;
+    std::cout << Diff<double,3>()(f, a) << std::endl;
+    std::cout << Diff<double,4>()(f, a) << std::endl;
+    std::cout << Diff<double,5>()(f, a) << std::endl;
 
     return 0;
 }
